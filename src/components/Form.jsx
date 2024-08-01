f20d73aeb4ea85fb320a6085cfdbd2b0904721a5
Sanket Verma <sanketv403@gmail.com>
2024-08-01 16:01:56 +0530

    form updated on 1 aug 2024 in main branch

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Form() {
-    function handleChange(){
-        console.log("changed occurred")
-    }
+  const [name, setName] = useState("");
+  const [obj, setObj] = useState({ name: '' });
+
+  function setObjName(event) {
+    event.preventDefault();
+    setObj({ name });
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <form>
-        <input onChange={handleChange} type="text" value="" />
+        <input onChange={handleChange} type="text" value={name} />
+        <button onClick={setObjName}>Submit</button>
+
+        <p>Name is {obj.name}</p>
       </form>
     </div>
-  )
+  );
 }
